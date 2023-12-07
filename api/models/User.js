9a1ad7450297a9e0727d9081909cb2526fc96c5f
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-12-07 11:39:35 +0000

    user model and post model created succesfully

---
+++ api/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    requred: true,
+  },
+  profilePicture: {
+    type: String,
+  },
+  joinDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  sentFollowRequest: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  receivedFollowRequests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationTaken: String,
+});
+
+const User = mongoose.models("User", userSchema);
+module.exports = User;
