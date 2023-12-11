dd25c401071555efcf7d320d16ef7a89b39b0494
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-12-11 11:50:51 +0000

    endpoint to like a post

--- api/index.js
+++ api/index.js
@@ -210,3 +210,31 @@ app.post("/create-post", async (req, res) => {
     res.status(500).json({ message: "Post creation failed" });
   }
 });
+
+//endpoint for liking a particular post
+app.put("/posts/:postId/:userId/like", async (req, res) => {
+  const postId = req.params.postId;
+  const userId = req.params.userId; // Assuming you have a way to get the logged-in user's ID
+
+  try {
+    const post = await Post.findById(postId).populate("user", "name");
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { $addToSet: { likes: userId } }, // Add user's ID to the likes array
+      { new: true } // To return the updated post
+    );
+
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    updatedPost.user = post.user;
+
+    res.json(updatedPost);
+  } catch (error) {
+    console.error("Error liking post:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while liking the post" });
+  }
+});
