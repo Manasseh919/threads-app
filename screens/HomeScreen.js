de93b6a03ed96246ae880a84a772dbe6ff2fa493
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-12-11 12:15:37 +0000

    getting the data for the all post in home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useContext, useEffect, useState } from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import jwtDecode from 'jwt-decode';
-import { UserType } from '../UserContext';
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import jwtDecode from "jwt-decode";
+import { UserType } from "../UserContext";
+import axios from "axios";
+import { Entypo, MaterialIcons, AntDesign,FontAwesome,Ionicons } from "@expo/vector-icons";
+
 
 const HomeScreen = () => {
-  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
   const { userId, setUserId } = useContext(UserType);
 
   useEffect(() => {
@@ -14,19 +17,108 @@ const HomeScreen = () => {
       const decodedToken = jwtDecode(token);
       const userId = decodedToken.userId;
       setUserId(userId);
-
-      
     };
 
     fetchUsers();
   }, []);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/get-posts");
+      setPosts(response.data);
+    } catch (error) {
+      console.log("Error fetching posts", error);
+    }
+  };
+  console.log("post", posts);
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <ScrollView style={{ marginTop: 50, flex: 1, backgroundColor: "white" }}>
+      <View style={{ alignItems: "center", marginTop: 20 }}>
+        <Image
+          style={{ width: 60, height: 40, resizeMode: "contain" }}
+          source={{
+            uri: "https://freelogopng.com/images/all_img/1688663386threads-logo-transparent.png",
+          }}
+        />
+      </View>
+      <View style={{ marginTop: 20 }}>
+        {posts?.map((post) => (
+          <View
+            style={{
+              padding: 15,
+              borderColor: "#D0D0D0",
+              borderTopWidth: 1,
+              flexDirection: "row",
+              gap: 10,
+              marginVertical: 10,
+            }}
+          >
+            <View>
+              <Image
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 20,
+                  resizeMode: "contain",
+                }}
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/128/149/149071.png",
+                }}
+              />
+            </View>
 
-export default HomeScreen
+            <View>
+              <Text
+                style={{ fontSize: 15, fontWeight: "bold", marginBottom: 4 }}
+              >
+                {post?.user?.name}
+              </Text>
+              <Text>{post?.content}</Text>
 
-const styles = StyleSheet.create({})
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 10,
+                  marginTop: 15,
+                }}
+              >
+                {post?.likes?.includes(userId) ? (
+                  <AntDesign
+                    onPress={() => handleDislike(post?._id)}
+                    name="heart"
+                    size={18}
+                    color="red"
+                  />
+                ) : (
+                  <AntDesign
+                    onPress={() => handleLike(post?._id)}
+                    name="hearto"
+                    size={18}
+                    color="black"
+                  />
+                )}
+
+                <FontAwesome name="comment-o" size={18} color="black" />
+
+                <Ionicons name="share-social-outline" size={18} color="black" />
+              </View>
+
+              <Text style={{ marginTop: 7, color: "gray" }}>
+                {post?.likes?.length} likes • {post?.replies?.length} reply
+              </Text>
+            </View>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
