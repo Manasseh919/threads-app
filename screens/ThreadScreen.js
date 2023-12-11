c995356a8dee5387e56837522392b96202045285
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-12-11 12:02:32 +0000

    share post connected to front end

--- screens/ThreadScreen.js
+++ screens/ThreadScreen.js
@@ -9,16 +9,29 @@ import {
 } from "react-native";
 import React, { useContext, useState } from "react";
 import { UserType } from "../UserContext";
+import axios from "axios";
 
 const ThreadScreen = () => {
   const [content, setContent] = useState("");
   const { userId, setUserId } = useContext(UserType);
 
-  const handlePostSubmit = () =>{
+  const handlePostSubmit = () => {
     const postData = {
-      userId
+      userId,
+    };
+    if (content) {
+      postData.content = content;
     }
-  }
+
+    axios
+      .post("http://localhost:4000/create-post", postData)
+      .then((response) => {
+        setContent("");
+      })
+      .catch((error) => {
+        console.log("error creating post", error);
+      });
+  };
   return (
     <SafeAreaView style={{ padding: 10 }}>
       <View
@@ -42,7 +55,7 @@ const ThreadScreen = () => {
         />
         <Text>Manasseh Ameyow</Text>
       </View>
-      <View style={{flexDirection:'row',marginLeft:10,}}>
+      <View style={{ flexDirection: "row", marginLeft: 10 }}>
         <TextInput
           value={content}
           onChangeText={(text) => setContent(text)}
@@ -52,10 +65,8 @@ const ThreadScreen = () => {
         />
       </View>
 
-      <View style={{marginTop:20}}/>
-      <Button  onPress={handlePostSubmit} title="Share Post"  />
-
-      
+      <View style={{ marginTop: 20 }} />
+      <Button onPress={handlePostSubmit} title="Share Post" />
     </SafeAreaView>
   );
 };
