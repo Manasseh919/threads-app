aceaa642a75967f46ba065a8ffac6f3afc0b9a98
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-12-11 11:34:48 +0000

    create post endoint

--- screens/ThreadScreen.js
+++ screens/ThreadScreen.js
@@ -1,14 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Button,
+} from "react-native";
+import React, { useState } from "react";
 
 const ThreadScreen = () => {
+  const [content, setContent] = useState("");
   return (
-    <View>
-      <Text>ThreadScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ padding: 10 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 10,
+          padding: 10,
+        }}
+      >
+        <Image
+          style={{
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/128/149/149071.png",
+          }}
+        />
+        <Text>Manasseh Ameyow</Text>
+      </View>
+      <View style={{flexDirection:'row',marginLeft:10,}}>
+        <TextInput
+          value={content}
+          onChangeText={(text) => setContent(text)}
+          placeholder="Type your message..."
+          placeholderTextColor={"black"}
+          multiline
+        />
+      </View>
 
-export default ThreadScreen
+      <View style={{marginTop:20}}/>
+      <Button title="Share Post" />
 
-const styles = StyleSheet.create({})
+      
+    </SafeAreaView>
+  );
+};
+
+export default ThreadScreen;
+
+const styles = StyleSheet.create({});
